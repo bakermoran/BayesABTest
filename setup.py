ae1a6ba1a75887d41c5bf19a4a06ac3d4b8ca8e1
godric <lahumez@gmail.com>
2022-05-17 11:05:04 +0200

    Bump numpy, pandas & seaborn versions

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ setup(
     include_package_data=True,
     python_requires='>=3.6',
     install_requires=[
-        'numpy==1.19.5',
-        'pandas==1.1.5',
-        'matplotlib==3.3.4',
-        'seaborn==0.10.1'
+        'numpy==1.22.3',
+        'pandas==1.4.2',
+        'matplotlib==3.5.2',
+        'seaborn==0.10.1',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
